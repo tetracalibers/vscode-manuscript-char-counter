b6fcb7a0aa6aa066ffde316ca05cd882b0ff176d
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-01-21 21:10:17 +0900

    helloWorldコマンドでリンクURL、コードブロック、フロントマターを除いた文字数をカウント

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,71 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode"
+import {
+  commands,
+  ExtensionContext,
+  StatusBarAlignment,
+  StatusBarItem,
+  TextDocument,
+  window
+} from "vscode"
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "markdown-char-counter" is now active!')
 
+  const charCounter = new CharCounter()
+
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("markdown-char-counter.helloWorld", () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World from MarkdownCharCounter!")
+  let disposable = commands.registerCommand("markdown-char-counter.helloWorld", () => {
+    charCounter.updateCount()
   })
 
+  context.subscriptions.push(charCounter)
   context.subscriptions.push(disposable)
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+class CharCounter {
+  #statusBarItem: StatusBarItem
+
+  constructor() {
+    this.#statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left)
+  }
+
+  dispose() {
+    this.#statusBarItem.dispose()
+  }
+
+  public updateCount() {
+    const editor = window.activeTextEditor
+    if (!editor) return this.#statusBarItem.hide()
+    const doc = editor.document
+    if (doc.languageId !== "markdown") this.#statusBarItem.hide()
+    const charCount = this.#getCharCount(doc)
+    this.#statusBarItem.text = charCount + (charCount !== 1 ? " Chars" : "Char")
+    this.#statusBarItem.show()
+  }
+
+  #getCharCount(doc: TextDocument) {
+    let docContent = doc.getText()
+    // コードブロック除外
+    docContent = docContent.replace(/```.*?\r?\n[\s\S]*?\r?\n```/g, "")
+    // frontmatter除外
+    docContent = docContent.replace(/---\r?\n[\s\S]*?\r?\n---/g, "")
+    // リンクURL除外
+    docContent = docContent.replace(/\[(.*?)\]\(.*?\)/g, "$1")
+    // 空白除去
+    docContent = docContent.replace(/\s/g, "")
+    // 改行文字除去
+    docContent = docContent.replace(/\r?\n/g, "")
+    //
+    return docContent.length
+  }
+}
